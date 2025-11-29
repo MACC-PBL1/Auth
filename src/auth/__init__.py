bc53a5d4472b3dce292b3ec3c1ecebdba1447465
gfernandez <gorka.fernandezg@alumni.mondragon.edu>
2025-11-29 11:15:27 +0100

    Changed back

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -61,7 +61,7 @@ async def lifespan(__app: FastAPI):
             service_port = int(os.getenv("PORT", "8000"))
             consul = ConsulClient(logger=logger)
             consul.register_service(
-                service_name="auth-service", 
+                service_name="auth", 
                 port=service_port, 
                 health_path="/auth/health"
             )
